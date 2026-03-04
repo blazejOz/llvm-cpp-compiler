2544c5058d5ab702d56e0feac97a7678f8587bc6
blazejOz <blazej.ozog@gmail.com>
2026-03-04 07:42:35 +0100

    feat: working MVP version from lexer to LLVM IR generator

--- include/AST.hpp
+++ include/AST.hpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Module.h>
+
 
 /**
  * @brief Base class for all expression nodes
@@ -11,17 +15,23 @@ class AST
 {
 public:
     virtual ~AST() = default;
+    virtual llvm::Value* codegen(llvm::LLVMContext& context, 
+                                 llvm::IRBuilder<>& builder, 
+                                 llvm::Module& module) = 0;
 };
 
 /**
  * @brief Represents a literal integer like '1' or '42'
  */
-class IntegerExprAST: public AST
+class IntegerExprAST : public AST 
 {
 private:
     int val_;
 public:
-    IntegerExprAST(int val): val_{val} {}
+    IntegerExprAST(int val);
+    llvm::Value* codegen(llvm::LLVMContext& context, 
+                         llvm::IRBuilder<>& builder, 
+                         llvm::Module& module) override;
 };
 
 /**
@@ -32,7 +42,9 @@ class PrintStmtAST: public AST
 private:
     std::unique_ptr<AST> expression_;
 public:
-    PrintStmtAST(std::unique_ptr<AST> expression)
-        : expression_(std::move(expression)) {}
+    PrintStmtAST(std::unique_ptr<AST> expression);
+    llvm::Value* codegen(llvm::LLVMContext& context, 
+                         llvm::IRBuilder<>& builder, 
+                         llvm::Module& module) override;
 };
 
